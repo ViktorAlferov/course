de5d277046666c56f2f47075c3c0906fbb608b6f
VictorAlferov <alferov1322@gmail.com>
2019-06-08 21:18:10 +0700

    [f] fix loadImage

--- home-work-1/difficult-task/loadImages.js
+++ home-work-1/difficult-task/loadImages.js
@@ -1,13 +1,13 @@
 let imagesPaths = ['/img/img_1.jpg', '/img/img_2.jpg', '/img/img_3.jpg', '/img/img_4.jpg']
 
-const loadImage = (paths) => {
+const loadImage = (path) => {
 	return new Promise(resolve => {
 		const img = new Image()
 		img.onload = () => {
-			return resolve(paths)
+			return resolve(path)
 		}
 		img.onerror = () => {
-			console.error(paths)
+			console.error(path)
         }
         img.src = paths
         document.body.appendChild(img)
